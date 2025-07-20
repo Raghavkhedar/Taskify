46e03fb4942e86a3d322cf8761bde7e431d97087
Raghav <rkhedar_be22@thapar.edu>
2025-07-21 04:15:52 +0530

    Add Clerk authentication components and middleware integration

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthPage = ({children} : {children:React.ReactNode}) => {
+    return (
+        <div className=" h-full flex items-center justify-center">
+            {children}
+        </div>
+    )
+}
+
+export default AuthPage;
---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,12 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
+const PLatformLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ClerkProvider>
+            {children}
+        </ClerkProvider>
+    )
+}
+
+export default PLatformLayout;  
+
